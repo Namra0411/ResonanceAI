60ce443df2ba71fe10fe66bb9a9425a8e967493a
kachhia Namra Dharmesh <202301129@dau.ac.in>
2026-07-09 15:49:55 +0530

    Remove Supabase client configuration

--- server/src/config/supabase.js
+++ server/src/config/supabase.js
@@ -1,12 +1,1 @@
-import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://esmyvgjrpvuazngwyjxp.supabase.co";
-const SUPABASE_SERVICE_KEY =
-  "***REMOVED***";
-
-const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_SERVICE_KEY
-);
-
-export default supabase;
